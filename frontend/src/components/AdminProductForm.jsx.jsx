f7a05590d5a697f9566280b07042eb6e87856ae9
rutvik2510 <rutvik010101@gmail.com>
2024-09-03 12:15:24 +0530

    add

--- frontend/src/components/AdminProductForm.jsx.jsx
+++ frontend/src/components/AdminProductForm.jsx.jsx
@@ -48,7 +48,7 @@ const AddProduct = () => {
     formData.append('quantity', quantity);
 
     try {
-      await axios.post('http://localhost:5000/api/products/createProduct', formData, {
+      await axios.post('http://localhost:5000/api/product/products', formData, {
         headers: {
           Authorization: `Bearer ${token}`,
           'Content-Type': 'multipart/form-data',
